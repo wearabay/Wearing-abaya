d7d6b5e6a89b7a85871ee4e8c787532cd992b249
wearabay <wearing.abaya99@gmail.com>
2026-08-06 14:20:52 +0000

    Remove duplicate navbar and footer from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import Navbar from "../components/layout/Navbar";
-import CartDrawer from "@/components/cart/CartDrawer";
+
 import Hero from "../components/home/Hero";
 import FeaturedCollections from "../components/home/FeaturedCollections";
 import FeaturedProducts from "../components/home/FeaturedProducts";
@@ -7,7 +6,6 @@ import JournalSection from "../components/home/JournalSection";
 import BrandValues from "../components/home/BrandValues";
 import InstagramGallery from "../components/home/InstagramGallery";
 import Newsletter from "../components/home/Newsletter";
-import Footer from "../components/layout/Footer";
 
 
 export default function Home() {
